b439f01761b5ad44b96259e349b5f57d938caf91
christopherchristensen <17888077+christopherchristensen@users.noreply.github.com>
2020-08-08 14:00:43 +0200

    add Collaborations component without implementation

---
+++ src/views/Collaborations.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<component class="mb-8"
+				   :is="_.startCase(article.template).split(' ').join('')"
+				   :key="article.id"
+				   :article="article"
+				   v-for="article of homeArticles" />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Collaborations"
+	};
+</script>
+
+<style scoped>
+
+</style>
