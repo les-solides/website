31aa0164db81390388e0b441aed917068875b39e
christopherchristensen <17888077+christopherchristensen@users.noreply.github.com>
2020-10-25 11:42:15 +0100

    redesign navigation

--- src/components/partials/NavigationSubfilter.vue
+++ src/components/partials/NavigationSubfilter.vue
@@ -1,36 +1,57 @@
 <template>
 	<div class="flex subfilter">
-		<Route class="mr-1" to="/products/jewelry/bracelets">bracelets, </Route>
-		<Route class="mr-1" to="/products/jewelry/cuffs">cuffs, </Route>
-		<Route class="mr-1" to="/products/jewelry/earrings">earrings, </Route>
-		<Route class="mr-1" to="/products/jewelry/hoops">hoops, </Route>
-		<Route class="mr-1" to="/products/jewelry/necklaces">necklaces, </Route>
-		<Route class="mr-1" to="/products/jewelry/rings">rings</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'bracelets' }"
+			   to="/products/jewelry/bracelets">bracelets,
+		</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'cuffs' }"
+			   to="/products/jewelry/cuffs">cuffs,
+		</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'earrings' }"
+			   to="/products/jewelry/earrings">earrings,
+		</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'hoops' }"
+			   to="/products/jewelry/hoops">hoops,
+		</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'necklaces' }"
+			   to="/products/jewelry/necklaces">necklaces,
+		</Route>
+		<Route class="mr-1"
+			   :class="{ 'text-gray-600': $route.params.variant !== 'rings' }"
+			   to="/products/jewelry/rings">rings
+		</Route>
 	</div>
 </template>
 
 <script>
 	import Route from "./Route";
+	
 	export default {
 		name: "NavigationSubfilter",
 		components: {Route}
 	};
 </script>
 
-<style scoped lang="scss">
-@import "./src/scss/partials/variables";
-.subfilter {
-	background: white;
-	left: 0;
-	padding: 0 1rem 1rem 0;
-	position: sticky;
-	top: 3rem;
-	width: 100%;
-	z-index: 10;
-	@media (min-width: $breakpoint-md) {
-		padding: 0 1rem 1rem;
-		position: fixed;
-		top: var(--header-height);
+<style scoped
+	   lang="scss">
+	@import "./src/scss/partials/variables";
+	
+	.subfilter {
+		background: white;
+		left: 0;
+		padding: 0 1rem 1rem 0;
+		position: sticky;
+		top: 3rem;
+		width: 100%;
+		z-index: 10;
+		@media (min-width: $breakpoint-md) {
+			padding: 0 1rem 0rem;
+			position: fixed;
+			top: calc(var(--header-height) + 1rem);
+		}
 	}
-}
 </style>
