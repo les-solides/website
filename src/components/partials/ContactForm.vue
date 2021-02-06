6d20c70aaf32064b076fed77511a1eea71f00862
christopherchristensen <17888077+christopherchristensen@users.noreply.github.com>
2021-02-06 14:37:25 +0100

    chore: styling contact form

--- src/components/partials/ContactForm.vue
+++ src/components/partials/ContactForm.vue
@@ -2,85 +2,89 @@
 	<form class=""
 		  @submit.prevent="contact">
 		<h1 class="mb-4">contact</h1>
-		<div class="flex w-full">
-			<div class="flex items-center md:w-1/4 w-1/2">
-				<label for="name" hidden></label>
-				<h1>name*</h1>
+		
+		<div class="form-inputs">
+			<div class="flex w-full">
+				<div class="flex items-center md:w-1/4 w-1/2">
+					<label for="name" hidden></label>
+					<h1>name*</h1>
+				</div>
+				<div class="flex items-center md:w-3/4 w-1/2">
+					<input id="name"
+						   class="special block placeholder-black text-black w-3/4"
+						   type="text"
+						   placeholder="name"
+						   v-model="name">
+				</div>
 			</div>
-			<div class="flex items-center md:w-3/4 w-1/2">
-				<input id="name"
-					   class="special block placeholder-black text-black w-3/4"
-					   type="text"
-					   placeholder="name"
-					   v-model="name">
+			
+			<div class="flex w-full">
+				<div class="flex items-center md:w-1/4 w-1/2">
+					<label for="email" hidden></label>
+					<h1>email*</h1>
+				</div>
+				<div class="flex items-center md:w-3/4 w-1/2">
+					<input id="email"
+						   class="special block placeholder-black text-black w-3/4"
+						   type="email"
+						   placeholder="email"
+						   v-model="email">
+				</div>
 			</div>
+			
+			<div class="flex w-full">
+				<div class="flex items-center md:w-1/4 w-1/2">
+					<label for="phone" hidden></label>
+					<h1>phone number</h1>
+				</div>
+				<div class="flex items-center md:w-3/4 w-1/2">
+					<input id="phone"
+						   class="special block placeholder-black text-black w-3/4"
+						   type="text"
+						   placeholder="phone number"
+						   v-model="phone">
+				</div>
+			</div>
+			
+			<div class="flex w-full">
+				<div class="flex items-center md:w-1/4 w-1/2">
+					<label for="order" hidden></label>
+					<h1>order number</h1>
+				</div>
+				<div class="flex items-center md:w-3/4 w-1/2">
+					<input id="order"
+						   class="special block placeholder-black text-black w-3/4"
+						   type="text"
+						   placeholder="#0000"
+						   v-model="order">
+				</div>
+			</div>
+			
+			<div class="flex w-full">
+				<div class="flex items-center md:w-1/4 w-1/2">
+					<label for="subject" hidden></label>
+					<h1>subject*</h1>
+				</div>
+				<div class="flex items-center md:w-3/4 w-1/2">
+					<input id="subject"
+						   class="special block placeholder-black text-black w-3/4"
+						   type="text"
+						   placeholder="subject"
+						   v-model="subject">
+				</div>
+			</div>
+			
+			<div class="divider my-4"></div>
+			
+			<label for="message"
+				   hidden></label>
+			<textarea id="message"
+					  class="block placeholder-black text-black w-full"
+					  placeholder="message ..."
+					  style="height: 200px"
+					  v-model="message"></textarea>
 		</div>
 		
-		<div class="flex w-full">
-			<div class="flex items-center md:w-1/4 w-1/2">
-				<label for="email" hidden></label>
-				<h1>email*</h1>
-			</div>
-			<div class="flex items-center md:w-3/4 w-1/2">
-				<input id="email"
-					   class="special block placeholder-black text-black w-3/4"
-					   type="email"
-					   placeholder="email"
-					   v-model="email">
-			</div>
-		</div>
-		
-		<div class="flex w-full">
-			<div class="flex items-center md:w-1/4 w-1/2">
-				<label for="phone" hidden></label>
-				<h1>phone number</h1>
-			</div>
-			<div class="flex items-center md:w-3/4 w-1/2">
-				<input id="phone"
-					   class="special block placeholder-black text-black w-3/4"
-					   type="text"
-					   placeholder="phone number"
-					   v-model="phone">
-			</div>
-		</div>
-		
-		<div class="flex w-full">
-			<div class="flex items-center md:w-1/4 w-1/2">
-				<label for="order" hidden></label>
-				<h1>order number</h1>
-			</div>
-			<div class="flex items-center md:w-3/4 w-1/2">
-				<input id="order"
-					   class="special block placeholder-black text-black w-3/4"
-					   type="text"
-					   placeholder="#0000"
-					   v-model="order">
-			</div>
-		</div>
-		
-		<div class="flex w-full">
-			<div class="flex items-center md:w-1/4 w-1/2">
-				<label for="subject" hidden></label>
-				<h1>subject*</h1>
-			</div>
-			<div class="flex items-center md:w-3/4 w-1/2">
-				<input id="subject"
-					   class="special block placeholder-black text-black w-3/4"
-					   type="text"
-					   placeholder="subject"
-					   v-model="subject">
-			</div>
-		</div>
-		
-		<div class="divider my-4"></div>
-		
-		<label for="message"
-			   hidden></label>
-		<textarea id="message"
-				  class="block placeholder-black text-black w-full"
-				  placeholder="message ..."
-				  style="height: 200px"
-				  v-model="message"></textarea>
 		
 		<input class="block mt-4 text-center"
 			   type="submit"
@@ -143,6 +147,11 @@
 			width: var(--column-width-default);
 		}
 	}
+	
+	.form-inputs h1 {
+		font-size: 1.25rem;
+	}
+	
 	.divider {
 		height: .5px;
 		width: 100%;
@@ -150,20 +159,20 @@
 	}
 	input[type=submit] {
 		background: none;
-		font-size: 1.75rem;
+		font-size: 1.25rem;
 		@media screen and (min-width: $breakpoint-md) {
 			font-size: 2.15rem;
 		}
 	}
 	input, textarea {
-		font-size: 1.75rem;
+		font-size: 1.25rem;
 		@media screen and (min-width: $breakpoint-md) {
 			font-size: 2.15rem;
 		}
 	}
 	::placeholder {
 		color: gray;
-		font-size: 1.75rem;
+		font-size: 1.25rem;
 		@media screen and (min-width: $breakpoint-md) {
 			font-size: 2.15rem;
 		}
