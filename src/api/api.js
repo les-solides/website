7aa94e09481a6354f61f55f3a851c7f2a0fbcaa6
christopherchristensen <17888077+christopherchristensen@users.noreply.github.com>
2022-01-29 12:15:04 +0100

    fix: add cors headers

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: process.env.VUE_APP_API_URL
+	baseURL: process.env.VUE_APP_API_URL,
+	headers: {
+		'Access-Control-Allow-Origin' : '*',
+		'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+	}
 });
